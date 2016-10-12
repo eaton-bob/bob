3a95f2f2a366745dd571e2bc680b33252bd0f871
Michal Vyskocil <michalvyskocil@eaton.com>
2016-10-12 10:50:52 +0200

    Problem: need to teach malamute
    Solution: add simple example in bob.c

---
+++ bob.c
@@ -0,0 +1,36 @@
+#include <malamute.h>
+
+int main () {
+
+    static char *endpoint = "inproc://bob";
+    int r;
+
+    zactor_t *server = zactor_new (mlm_server, "Malamute");
+    zstr_sendx (server, "BIND", endpoint, NULL);
+
+    mlm_client_t *producer = mlm_client_new ();
+    r = mlm_client_connect (producer, endpoint, 1000, "producer");
+    assert (r != -1);
+
+    mlm_client_t *consumer = mlm_client_new ();
+    r = mlm_client_connect (consumer, endpoint, 1000, "consumer");
+    assert (r != -1);
+
+
+    mlm_client_set_producer (producer, "BOB");
+    mlm_client_set_consumer (consumer, "BOB", ".*");
+
+    mlm_client_sendx (producer, "SUBJECT", "Hello", "Bob", NULL);
+
+    zmsg_t *msg = mlm_client_recv (consumer);
+    zsys_info ("subject=%s", mlm_client_subject (consumer));
+    zsys_info ("command=%s", mlm_client_command (consumer));
+    zmsg_print (msg);
+    zmsg_destroy (&msg);
+
+
+    mlm_client_destroy (&consumer);
+    mlm_client_destroy (&producer);
+    zactor_destroy (&server);
+
+}
